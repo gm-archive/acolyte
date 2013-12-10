1669a6a2e62f823a9eee880476fda6bb3bd358eb
Kyle J. Van Berendonck <kvanberendonck@gmail.com>
2013-12-10 14:45:24 +1100

    Added event.hpp/event.cpp

---
+++ packages/acolyte-rt/include/ert/event.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2013 Acolyte Strike Force. All rights reserved.
+// Use of this source code is governed by a BSD3-style license that can be found in the LICENSE file.
+
+#ifndef ERT_EVENT_HPP_
+#define ERT_EVENT_HPP_
+
+#include "ert/object.hpp"
+
+namespace ert {
+  enum event_t {
+    // TODO: events from GM:S documentation
+    ev_placeholder
+  };
+
+  struct event {
+    typedef object::id_t metadata_t;
+    event_t type;
+    metadata_t metadata;
+  };
+}
+
+#endif // ERT_EVENT_HPP_

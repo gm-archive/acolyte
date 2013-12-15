d4a1df70e92862d4faf0c4ebbb584234c960d7a3
Kyle J. Van Berendonck <kvanberendonck@gmail.com>
2013-12-16 00:03:12 +1100

    Fixed missing conversions from ert to art in test suite

--- packages/acolyte-rt/test/test_math.cpp
+++ packages/acolyte-rt/test/test_math.cpp
@@ -5,25 +5,25 @@
 
 #include "art/math.hpp"
 
-TEST(ert_floor_test, floor_whole)
+TEST(art_floor_test, floor_whole)
 {
-  EXPECT_EQ(ert::floor(0), 0);
-  EXPECT_EQ(ert::floor(10), 10);
-  EXPECT_EQ(ert::floor(100), 100);
+  EXPECT_EQ(art::floor(0), 0);
+  EXPECT_EQ(art::floor(10), 10);
+  EXPECT_EQ(art::floor(100), 100);
 }
 
-TEST(ert_floor_test, floor_decimal)
+TEST(art_floor_test, floor_decimal)
 {
-  EXPECT_EQ(ert::floor(3.3), 3);
-  EXPECT_EQ(ert::floor(4.5), 4);
-  EXPECT_EQ(ert::floor(5.7), 5);
+  EXPECT_EQ(art::floor(3.3), 3);
+  EXPECT_EQ(art::floor(4.5), 4);
+  EXPECT_EQ(art::floor(5.7), 5);
 }
 
-TEST(ert_floor_test, floor_negative)
+TEST(art_floor_test, floor_negative)
 {
-  EXPECT_EQ(ert::floor(-0), 0);
-  EXPECT_EQ(ert::floor(-1), -1);
-  EXPECT_EQ(ert::floor(-1.3), -2);
-  EXPECT_EQ(ert::floor(-2.5), -3);
-  EXPECT_EQ(ert::floor(-3.7), -4);
+  EXPECT_EQ(art::floor(-0), 0);
+  EXPECT_EQ(art::floor(-1), -1);
+  EXPECT_EQ(art::floor(-1.3), -2);
+  EXPECT_EQ(art::floor(-2.5), -3);
+  EXPECT_EQ(art::floor(-3.7), -4);
 }

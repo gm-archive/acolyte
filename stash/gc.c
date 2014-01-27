e78bb4761b432197bc8a852fcd7b9f0e9935fd60
Kyle Van Berendonck <kvanberendonck@gmail.com>
2014-01-27 23:35:05 +1100

    Premature work on GC

---
+++ stash/gc.c
@@ -0,0 +1,4 @@
+// Copyright (c) 2013 Acolyte Strike Force. All rights reserved.
+// Use of this source code is governed by a BSD3-style license which can be found in the LICENSE file.
+
+#include "gc.h"
---
+++ stash/gc.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2013 Acolyte Strike Force. All rights reserved.
+// Use of this source code is governed by a BSD3-style license which can be found in the LICENSE file.
+
+#ifndef _ACOLYTE_GC_H_
+#define _ACOLYTE_GC_H_
+
+#include <stdint.h>
+
+typedef struct superblock superblock_t;
+
+struct superblock {
+  superblock_t *next;
+  void *base;
+};
+
+typedef struct block block_t;
+
+struct block {
+  //
+};
+
+typedef struct alloc alloc_t;
+
+struct alloc {
+  union {
+    uint16_t meta;
+    struct {
+      unsigned link : 13;
+      unsigned unused : 1;
+      unsigned type : 2;
+    };
+  };
+};
+
+#endif // _ACOLYTE_GC_H_
